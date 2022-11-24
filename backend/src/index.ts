c52ebbbd32f0fd183330a595e1f014081d39db5e
Alexander Kleimionov <Kleimionov.Alexander@gmail.com>
2022-11-24 16:49:42 +0300

    Table relations is changed. Admin is creating by default when initialize DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import server from "./server";
 import database from "./database";
 import { initModels } from "./models";
+import { initialDBInitialization } from "./utils/initialDBScript";
 
 (async () => {
   try {
     await database.connection();
-    initModels();
+    await initModels();
+    await initialDBInitialization();
+
     server.start();
   } catch (error) {
     console.log(`Something went wrong: ${error}`);
