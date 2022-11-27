1dda5e2ba3319fef9a1aa0e4ee5759113a95a036
Alexander Kleimionov <Kleimionov.Alexander@gmail.com>
2022-11-27 12:27:26 +0300

    Color to constants

--- frontend/src/constants/status.ts
+++ frontend/src/constants/status.ts
@@ -1,9 +1,11 @@
 import { IStatus } from 'types/status'
 
+import colors from 'styles/variables/_colors.module.scss'
+
 export const statusColor = {
-  purple: { background: '#806194', color: '#fff' },
-  pink: { background: '#DF1674', color: '#fff' },
-  green: { background: '#C2F970', color: '#333' }
+  purple: { background: colors.lightPurple, color: colors.white },
+  pink: { background: colors.darkPink, color: colors.white },
+  green: { background: colors.lightGreen, color: colors.darkGrey }
 }
 
 export const userStatues: Record<string, IStatus> = {
