afa3a233d3ee99d4678b62975e455ea4f94ed285
Alexander Kleimionov <Kleimionov.Alexander@gmail.com>
2022-11-29 18:27:08 +0300

    Rollback passport auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,16 +1,20 @@
-import Router from "express";
+import Router, { NextFunction, Request, Response } from "express";
 import passport from "passport";
 
 const router = Router();
 
+const checkPassport = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate("local", (error, user, info) => {
+    if (error) return next(error); // It is null
+    if (!user) return res.status(401).send(info.message);
+    return res.status(200).send(user);
+  })(req, res, next);
+};
+
 // Login with passport. Access token will be added to the cookies
-router.post("/login", async (req, res, next) => {
+router.post("/login", passport.authenticate("local"), async (req, res) => {
   try {
-    passport.authenticate("local", (error, user, info) => {
-      if (error) return next(error); // It is null
-      if (!user) return res.status(401).send(info.message);
-      res.status(200).send(user);
-    })(req, res, next);
+    res.status(200).send(req.user);
   } catch (error) {
     res.status(500).send(error);
   }
